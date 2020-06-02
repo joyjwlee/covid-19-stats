ac5ab95500acb66e79693c4938eba9821319660d
Jaewon Lee <joyjwlee@gmail.com>
2020-06-02 07:58:39 -0700

    google analytics

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ function App() {
   return (
     <div>
       <br />
-      <h2 style={{ textAlign: "center" }}>COVID-19 Real-Time Updates</h2>
+      <h2 style={{ textAlign: "center" }}>COVID-19 Live Updates</h2>
       <h4 style={{ textAlign: "center" }}>Made by Jaewon Lee</h4>
       <br />
       <CardDeck>
