ad2f0a34d94df017f7aefb31f471c8773c2ffc1b
Jaewon Lee <joyjwlee@gmail.com>
2020-06-01 18:07:08 -0700

    Updates

---
+++ precache-manifest.93fb5ee7ecb290e366e4c6794f5afdba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3a515faab25291a0fcdd517179d0f8",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "e5e2ce43d504618c388f",
+    "url": "/covid-19-stats/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "651e73942722fcbe4313",
+    "url": "/covid-19-stats/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e5e2ce43d504618c388f",
+    "url": "/covid-19-stats/static/js/2.0a2a33ec.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-19-stats/static/js/2.0a2a33ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "651e73942722fcbe4313",
+    "url": "/covid-19-stats/static/js/main.1cb67dc0.chunk.js"
+  },
+  {
+    "revision": "b54e26ce7d40c88ceed2",
+    "url": "/covid-19-stats/static/js/runtime-main.4048ae80.js"
+  }
+]);
